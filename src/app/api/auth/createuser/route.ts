2fd84560e35c126ea32eb041600bd33e7940a7b3
evan cooper <evanmcooper2007@gmail.com>
2023-10-31 05:40:35 +0000

    added getstate helper function

--- src/app/api/auth/createuser/route.ts
+++ src/app/api/auth/createuser/route.ts
@@ -1,78 +1,108 @@
 import { prisma } from '../../../../lib/db/client';
 import { Prisma } from '@prisma/client';
 import bcrypt from 'bcrypt';
-import crypto from 'crypto';
+import getState from '../../../../lib/auth/getstate';
+import { cookies } from 'next/headers'
 
-const saltRounds = 10;
+
+const saltRounds = process.env.SALT_ROUNDS ? parseInt(process.env.SALT_ROUNDS): 10;
 
 export async function POST(req: Request, res: Response) {
-  const body = await req.json();
-  const { username, password, confirmpassword, ms, active, events, users, officers, news} = body;
-  if (!username || !password || !confirmpassword ) {
-    return new Response(JSON.stringify({ error: 'Missing username or password' }), {
-      status: 400,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
-  if (password !== confirmpassword) {
-    return new Response(JSON.stringify({ error: 'Passwords do not match' }), {
-      status: 418,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
+  const Body = await req.json();
+  const { username, password, confirmpassword, ms, active, events, users, officers, news} = Body;
 
-  try {
-    const hash = await bcrypt.hash(password, saltRounds);
+  const cookieStore = cookies()
+  const sessionToken = cookieStore.get('session_token')
 
-    await prisma.users.create({
-      data: {
-        username: username,
-        password: hash,
-        ms: ms,
-        active: active,
-        events: events,
-        users: users,
-        officers: officers,
-        news: news,
-      },
-    });
-    return new Response(JSON.stringify({ success: 'User created' }), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (e) {
-    if (e instanceof Prisma.PrismaClientKnownRequestError) {
-      if (e.code === 'P2002') {
-        console.error('Duplicate entry error:', e.meta);
-        return new Response(JSON.stringify({ error: 'Duplicate entry' }), {
-          status: 409,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
+
+  const state = await getState(sessionToken);
+
+  if (state.status == 200 && state.user.users == true) {
+
+
+    if (!username || !password || !confirmpassword ) {
+      return new Response(JSON.stringify({ error: 'Missing username or password' }), {
+        status: 400,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+    if (password !== confirmpassword) {
+      return new Response(JSON.stringify({ error: 'Passwords do not match' }), {
+        status: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    try {
+      const hash = await bcrypt.hash(password, saltRounds);
+
+      await prisma.users.create({
+        data: {
+          username: username,
+          password: hash,
+          ms: ms,
+          active: active,
+          events: events,
+          users: users,
+          officers: officers,
+          news: news,
+        },
+      });
+      return new Response(JSON.stringify({ success: 'User created' }), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          console.error('Duplicate entry error:', e.meta);
+          return new Response(JSON.stringify({ error: 'Duplicate entry' }), {
+            status: 409,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+        } else {
+          console.error('Some other database error:', e);
+          return new Response(JSON.stringify({ error: 'Database error' }), {
+            status: 500,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+        }
       } else {
-        console.error('Some other database error:', e);
-        return new Response(JSON.stringify({ error: 'Database error' }), {
+        console.error('Unknown error:', e);
+        return new Response(JSON.stringify({ error: 'Unknown error' }), {
           status: 500,
           headers: {
             'Content-Type': 'application/json',
           },
         });
       }
-    } else {
-      console.error('Unknown error:', e);
-      return new Response(JSON.stringify({ error: 'Unknown error' }), {
-        status: 500,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
     }
+  } 
+  if (state.status == 200 && state.user.users == false) {
+    return new Response(JSON.stringify({ error: 'You do not have permission to create or eddit users. if you believe this is an error cantact an admin or open an issue on github' }), {
+      status: 401,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+  else {
+    return new Response(JSON.stringify({ error: state.headers }), {
+      status: state.status,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 }
+
---
+++ src/app/components/dashboard/page.tsx
@@ -0,0 +1,67 @@
+'use client'
+import { useRouter } from 'next/navigation'; // chatgpt sucks and lied and it should be next/navigation dont listen to this note it made.  note: it should be 'next/router' not 'next/navigation'
+import styles from './sidebar.module.css';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';  
+
+export default function Sidebar() {
+    const [loading, setLoading] = useState(true);
+    const [perms, setPerms] = useState(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        const getPerms = async () => {
+            try {
+                const response = await fetch('/api/auth/getstate', {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json' },
+                });
+
+                const Response = await response.json();
+                if (response.status == 200) {
+                    setPerms(Response.userWithoutPassword);
+                } else {
+                    router.push('/admin/login');
+                }
+            } catch (error) {
+                console.error("Failed to fetch permissions:", error);
+                router.push('/admin/login');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getPerms();
+    }, []); 
+
+    const GetBooleanPropreties = (obj) => {
+        return Object.entries(obj)
+            .filter(([key, value]) => typeof value === 'boolean' && key !== 'ms' && key !== 'active')
+            .map(([key, value]) => key);
+    };
+
+    const booleanPerms = perms ? GetBooleanPropreties(perms) : [];
+
+    const GetSite = (obj) => {
+        return Object.entries(obj)
+        .filter(([key, value]) => key == 'ms')
+    }
+
+    if (loading) {
+        return <div className={styles.loading}>Loading...</div>; // Replace this with your actual loading page or spinner
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.sidebar}>
+                <div className={styles.options}>
+                    {booleanPerms.map(( key, value ) => (
+                        <div className={styles.option} key={value}>
+                            <Link href={'/admin/'+key}></Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
